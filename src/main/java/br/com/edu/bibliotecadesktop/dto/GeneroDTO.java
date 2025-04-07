a84165466c93cf7093208d5eadaecd96ff6ad4ae
alec-san <116175872+alec-san@users.noreply.github.com>
2025-04-07 15:07:43 -0300

    Add files via upload

---
+++ src/main/java/br/com/edu/bibliotecadesktop/dto/GeneroDTO.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.edu.bibliotecadesktop.dto;
+
+/**
+ *
+ * @author Alecksandre
+ */
+public class GeneroDTO {
+
+    public Long id;
+    public String nome;
+    public String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
